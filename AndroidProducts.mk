ivanmeler/android_device_samsung_n7100

n7100: namelessify
Change-Id: I312ead99673134305f64e134964282ad2bc00231

AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_n7100.mk
+    $(LOCAL_DIR)/full_n7100.mk \
+    $(LOCAL_DIR)/nameless_n7100.mk

DerTeufel/android_device_samsung_t0lte

Conflicts:
	AndroidProducts.mk
	BoardConfig.mk
	cm.dependencies
	cm.mk
	common.mk
	full_t0lte.mk

AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_t0lte.mk
+    $(LOCAL_DIR)/full_t0lte.mk \
+    $(LOCAL_DIR)/nameless_t0lte.mk